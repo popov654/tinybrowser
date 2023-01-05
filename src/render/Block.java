bcc658af9e7c6b27468e8763efaf28cba111e507
popov654 <popov654@yandex.ru>
2023-01-05 21:44:55 +0300

    Fixed space in Block.toString method

--- src/render/Block.java
+++ src/render/Block.java
@@ -3795,7 +3795,7 @@ public class Block extends JPanel implements Drawable, MouseListener, MouseMotio
         s += "ID " + (id != null && id.length() > 0 ? id : "<none>") + " ";
         s +=  "(" + width + "x" + height + ")";
         if (textContent != null && textContent.length() > 0) {
-            s += "content: \"" + textContent + "\"";
+            s += " content: \"" + textContent + "\"";
         }
         return s;
     }
