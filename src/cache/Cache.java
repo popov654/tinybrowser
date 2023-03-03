c1f9eef43026546789014d28ee4181227fde5296
popov654 <popov654@yandex.ru>
2023-03-03 10:34:26 +0300

    Added no-op cache implementation

---
+++ src/cache/Cache.java
@@ -0,0 +1,17 @@
+package cache;
+
+/**
+ *
+ * @author Alex
+ */
+public class Cache {
+
+    public boolean contains(String url) {
+        return true;
+    }
+
+    public String get(String url) {
+        return url;
+    }
+
+}
