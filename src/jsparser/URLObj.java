48ee47055b676fc7fe005e31264670be02df9f73
popov654 <popov654@yandex.ru>
2023-06-11 05:56:09 +0300

    Added revokeObjectURL method

--- src/jsparser/URLObj.java
+++ src/jsparser/URLObj.java
@@ -1,11 +1,9 @@
 package jsparser;
 
-import java.awt.event.ActionEvent;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.UUID;
 import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import network.Request;
 
 /**
@@ -46,8 +44,37 @@ public class URLObj extends JSObject {
         }
     }
 
+    class revokeObjectURLFunction extends Function {
+        @Override
+        public JSValue call(JSObject context, Vector<JSValue> args, boolean as_constr) {
+            if (args.size() == 0) {
+                JSError e = new JSError(null, "Arguments error: 1 argument expected", getCaller().getStack());
+                getCaller().error = e;
+                return Undefined.getInstance();
+            }
+
+            JSObject window = (JSObject) Block.getVar("window", getCaller());
+            JSObject location = (JSObject) window.get("location");
+            String url = location.get("href").asString().getValue();
+
+            URL uri = null;
+            try {
+                if (!url.isEmpty() && !url.matches("^(https?|ftp|file):")) {
+                    url = "file:///" + url;
+                }
+                uri = new URL(url);
+            } catch (MalformedURLException ex) {}
+
+            String uuid = args.get(0).asString().getValue().split("://")[1];
+            Request.removeBlob(uri, UUID.fromString(uuid));
+
+            return Undefined.getInstance();
+        }
+    }
+
     private URLObj() {
         items.put("createObjectURL", new createObjectURLFunction());
+        items.put("revokeObjectURL", new revokeObjectURLFunction());
     }
 
     public static URLObj getInstance() {
