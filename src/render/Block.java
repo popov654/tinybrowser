07e34a86c4a25df5e75ccb1297d11b1feef33040
popov654 <popov654@yandex.ru>
2023-07-22 06:33:31 +0300

    Fixed typo in regular expression

--- src/render/Block.java
+++ src/render/Block.java
@@ -7248,7 +7248,7 @@ public class Block extends JPanel implements Drawable, MouseListener, MouseMotio
         }
 
         if (prop.equals("background-position-y")) {
-            if (value.matches("^[0-9.]+[0-9](px|em|rem|%|v(w|h|min|max)$")) {
+            if (value.matches("^[0-9.]+[0-9](px|em|rem|%|v(w|h|min|max))$")) {
                 int[] val = parseValueStringToArray(value);
                 setBackgroundPositionY(val[0], val[1]);
             }
